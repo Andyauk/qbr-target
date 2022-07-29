e3987509c41ca5a14d813339b1a8efc02fe31ab9
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2022-07-29 12:19:08 -0500

    Config: Add Toggle

--- config.lua
+++ config.lua
@@ -1,6 +1,7 @@
 local Config, Players, Types, Entities, Models, Zones, Bones, PlayerData = {}, {}, {}, {}, {}, {}, {}, {}
 Types[1], Types[2], Types[3] = {}, {}, {}
 Config.VehicleBones = {'chassis', 'windscreen', 'seat_pside_r', 'seat_dside_r', 'bodyshell', 'suspension_lm', 'suspension_lr', 'platelight', 'attach_female', 'attach_male', 'bonnet', 'boot', 'chassis_dummy', 'chassis_Control', 'door_dside_f', 'door_dside_r', 'door_pside_f', 'door_pside_r', 'Gun_GripR', 'windscreen_f', 'VFX_Emitter', 'window_lf', 'window_lr', 'window_rf', 'window_rr', 'engine', 'gun_ammo', 'ROPE_ATTATCH', 'wheel_lf', 'wheel_lr', 'wheel_rf', 'wheel_rr', 'exhaust', 'overheat', 'misc_e', 'seat_dside_f', 'seat_pside_f', 'Gun_Nuzzle'}
+Config.Toggle = false -- True: Toggle Target || False: Show Target While Holding L ALT
 
 -------------------------------------------------------------------------------
 -- Settings
